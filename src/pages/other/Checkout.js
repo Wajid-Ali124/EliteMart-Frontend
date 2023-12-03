7ba826f9c89c5667fd2cf1d1616ff245f47700b0
Wajid Ali <99598560+Wajid-Ali124@users.noreply.github.com>
2023-12-03 10:22:55 +0500

    Update Checkout.js

--- src/pages/other/Checkout.js
+++ src/pages/other/Checkout.js
@@ -5,6 +5,7 @@ import { getDiscountPrice } from "../../helpers/product";
 import SEO from "../../components/seo";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
+import Form from "../../components/checkout/Form";
 
 const Checkout = () => {
   let cartTotalPrice = 0;
@@ -28,93 +29,7 @@ const Checkout = () => {
           <div className="container">
             {cartItems && cartItems.length >= 1 ? (
               <div className="row">
-                <div className="col-lg-7">
-                  <div className="billing-info-wrap">
-                    <h3>Billing Details</h3>
-                    <div className="row">
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>First Name</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Last Name</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-info mb-20">
-                          <label>Company Name</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-select mb-20">
-                          <label>Country</label>
-                          <select>
-                            <option>Select a country</option>
-                            <option>Pakistan</option>
-                            <option>USA</option>
-                            <option>UAE</option>
-                            <option>Saudia</option>
-                            <option>Iran</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-info mb-20">
-                          <label>Street Address</label>
-                          <input className="billing-address" placeholder="House number and street name" type="text" />
-                          <input placeholder="Apartment, suite, unit etc." type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-info mb-20">
-                          <label>Town / City</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>State / County</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Postcode / ZIP</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Phone</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Email Address</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="additional-info-wrap">
-                      <h4>Additional information</h4>
-                      <div className="additional-info">
-                        <label>Order notes</label>
-                        <textarea
-                          placeholder="Notes about your order, e.g. special notes for delivery. "
-                          name="message"
-                          defaultValue={""}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <Form />
 
                 <div className="col-lg-5">
                   <div className="your-order-area">
@@ -170,9 +85,6 @@ const Checkout = () => {
                         </div>
                       </div>
                       <div className="payment-method"></div>
-                    </div>
-                    <div className="place-order mt-25">
-                      <button className="btn-hover">Place Order</button>
                     </div>
                   </div>
                 </div>
